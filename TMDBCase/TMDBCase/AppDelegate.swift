f29f9c70625452e93bc93114bb3d0cadea461aab
Ahmet Sina Ustem <ahmet.sina@applogist.com>
2020-12-16 22:39:22 +0300

    Initial configs cleaned

---
+++ TMDBCase/TMDBCase/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TMDBCase
+//
+//  Created by AppLogist on 16.12.2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ TMDBCase/TMDBCase/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TMDBCase
+//
+//  Created by AppLogist on 16.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
