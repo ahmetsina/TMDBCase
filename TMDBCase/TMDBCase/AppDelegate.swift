0d29f39127331be756e546c1ad77a7289518cbda
Ahmet Sina Ustem <ahmet.sina@applogist.com>
2020-12-18 01:26:41 +0300

    Added first ui elements to Main Screen

--- TMDBCase/TMDBCase/AppDelegate.swift
+++ TMDBCase/TMDBCase/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let bounds = UIScreen.main.bounds
         self.window = UIWindow(frame: bounds)
-        let viewController = MainScreenViewController()
+        let viewController = MainScreenViewController(viewModel: MainScreenViewModel())
         self.window?.rootViewController = UINavigationController(rootViewController: viewController)
         self.window?.makeKeyAndVisible()
         return true
--- TMDBCase/TMDBCase/ViewControllers/MainScreenViewController.swift
+++ TMDBCase/TMDBCase/ViewControllers/MainScreenViewController.swift
@@ -9,13 +9,71 @@ import UIKit
 
 final class MainScreenViewController: BaseViewController<MainScreenViewModel> {
     
+    private let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        viewModel.getConfig { (errorMessage) in
+            if let errorMessage = errorMessage {
+                debugPrint(errorMessage)
+                return
+            }
+            debugPrint("Success")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateSearchController()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        searchController.isActive = false
     }
     
     private func setupUI(){
+        setupNavigationBar()
+        
+    }
+    
+    private func updateSearchController(){
+        searchController.searchBar.placeholder = "Search.."
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        if #available(iOS 11.0, *) {
+            navigationItem.searchController = searchController
+        }else {
+            navigationItem.titleView = searchController.searchBar
+        }
+        definesPresentationContext = true
+    }
+    
+    private func setupNavigationBar(){
         title = "Popular Movies"
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = true
+        }
     }
 }
 
+
+// MARK: - UISearchResultsUpdating
+
+extension MainScreenViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let searchText = searchController.searchBar.text,
+              !searchText.isEmpty,
+              searchController.isActive  else {
+            //viewModel.update(filteredList: [])
+            /// - TODO: Reload Data
+            return
+        }
+        
+        //filterContentForSearchText(searchText)
+        //updateTableViewData()
+        debugPrint(searchText)
+    }
+}
